97b5939e187a03ccf37236ea1c18baf7bdb2e148
stas33 <you@example.com>
2021-11-08 13:01:16 +0200

    edited settings.py-4

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '13.80.177.112']
 
 # AUTH_USER_MODEL = "invmanagement.Order"
 
