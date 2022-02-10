4c6ead4545f497d80107d2c661a525d55f723353
stas33 <you@example.com>
2022-02-10 19:04:42 +0200

    edited product update-17

--- products/views.py
+++ products/views.py
@@ -75,7 +75,7 @@ def update_product(request, pk):
     queryset = Product.objects.get(id=pk)
     #categ = queryset.category
     categ = Product.objects.get(id=pk, category__id__in=categories)
-    id = categ.category_id
+    id = categ.category.id
     form = ProductUpdateForm(instance=queryset)
     if request.method == 'POST':
         form = ProductUpdateForm(request.POST, instance=queryset)
